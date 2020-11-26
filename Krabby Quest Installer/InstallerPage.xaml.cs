1cf1a41de55e075c48be6c279bea07100e681102
Jeremy Glazebrook <xxjdrocks450xx@hotmail.com>
2020-11-26 17:17:38 -0500

    Installer BETA Game patching added Launcher screen fixes New Logos New Icons Preparing for first BETA release!

--- Krabby Quest Installer/InstallerPage.xaml.cs
+++ Krabby Quest Installer/InstallerPage.xaml.cs
@@ -22,6 +22,7 @@ namespace KrabbyQuestInstaller
     public partial class InstallerPage : Page
     {
         StringBuilder StandardOutput = new StringBuilder();
+        StringBuilder Log = new StringBuilder();
         private enum InstallerState
         {
             /// <summary>
@@ -39,9 +40,17 @@ namespace KrabbyQuestInstaller
             /// <summary>
             /// Waiting for blender setup to complete
             /// </summary>
-            WaitingForCompletion3D
+            WaitingForCompletion3D,
+            /// <summary>
+            /// Asking to go to launcher
+            /// </summary>
+            GoToLauncher,
         }
         private InstallerState CurrentState;
+        private bool isPartialInstall = false;
+        private string partialTitle;
+        private AutoInstall.InstallationType PartialType;
+
         public InstallationCompletionInfo[] StepInfo = new InstallationCompletionInfo[5];
         public AutoInstall Installation;
         public bool Installing = false;
@@ -156,7 +165,7 @@ namespace KrabbyQuestInstaller
             config.DestinationPath = WorkspaceDir.Text;
             config.SourcePath = InstallPath.Text;
             config.Save();
-        }
+        }        
 
         void StartInstallation(StinkyFile.Installation.AutoInstall.InstallationType Type = default)
         {
@@ -175,7 +184,7 @@ namespace KrabbyQuestInstaller
             //started text
             var textBlock = new TextBlock()
             {
-                Text = $"{(Type == default ? "Complete" : Enum.GetName(typeof(StinkyFile.Installation.AutoInstall.InstallationType), Type))}" +
+                Text = $"{(Type == default ? "Full" : AutoInstall.GetFriendlyStepName(Type))}" +
                         $" Installation starting...",
                 Foreground = Brushes.DarkCyan,
                 FontWeight = FontWeights.Bold,
@@ -192,11 +201,19 @@ namespace KrabbyQuestInstaller
                                         ExtractTarGA(tuple.path, tuple.chunk, tuple.success),
                 OnFontCreate = (object s, (string path, RefPrim<bool> success) tuple) =>
                                         RunFontConverter(tuple.path, tuple.success),
-                OnProgressChanged = (object s, InstallationCompletionInfo info) => InstallationInformationChanged(info),                
-                OnStepStarted = StepStarted
+                OnProgressChanged = (object s, InstallationCompletionInfo info) => InstallationInformationChanged(info),
+                OnStepStarted = StepStarted,
+                OnErrorRaised = (object s, string error) => SafePushError(error, Brushes.Red)
             };
             Installation.OnPausedStateChanged += Installation_OnPausedStateChanged;
             Installing = true;
+            if (Type != default)
+            {
+                isPartialInstall = true;
+                partialTitle = AutoInstall.GetFriendlyStepName(Type);
+                PartialType = Type;
+            }
+            else isPartialInstall = false;
             //run async task
             Task.Run(() =>
             {
@@ -208,9 +225,12 @@ namespace KrabbyQuestInstaller
 
         private void Installation_OnPausedStateChanged(object sender, bool e)
         {
-            if (!e)
-                PauseButton.Content = "\xE769;";
-            else PauseButton.Content = "\xE768;";
+            Dispatcher.Invoke(delegate
+            {
+                if (!e)
+                    PauseButton.Content = "\xE769;";
+                else PauseButton.Content = "\xE768;";
+            });
         }
 
         /// <summary>
@@ -251,6 +271,7 @@ namespace KrabbyQuestInstaller
 #if DEBUG
                     PushError(info.StandardOutput, Brushes.Gray);
 #endif
+                    Log.AppendLine(info.StandardOutput);
                     StandardOutput.AppendLine(info.StandardOutput);
                 }
                 InstallationGrid.Visibility = Visibility.Visible;
@@ -263,26 +284,45 @@ namespace KrabbyQuestInstaller
             });
         }
 
+        void ShowSuccessMessage()
+        {
+            CurrentState = InstallerState.GoToLauncher;
+            var title = (!isPartialInstall ? "Installation" : partialTitle) + " Complete!";
+            ShowMessage(title,
+                "The installation seems good so far, but to be sure, you should hop into the game " +
+                "and try it out! \n" +
+                "\n" +
+                "Use the 'Go to Launcher' button to be taken to the launch screen.",
+                "Go to Launcher",
+                "All set");
+        }
+
         void OnInstallationCompleted(Task<bool> task)
         {
             Installing = false;
-            bool success = task.Result;
-            var exception = task.Exception;
-            var errors = Installation.Errors.ToList();
-            if (exception != null)
-                errors.Add(exception.Message);
+            bool success = false;
             Dispatcher.Invoke(delegate
             {
+                try
+                {
+                    success = task.Result;
+                    var exception = task.Exception;
+                    var errors = Installation.Errors.ToList();
+                    if (exception != null)
+                        PushError(exception.Message);
+                }
+                catch (Exception e)
+                {
+                    PushError(e.Message);
+                }
                 PreferencesGrid.IsEnabled = true;
                 if (success)
                     InstallationGrid.Visibility = Visibility.Hidden;
-                else
-                    PushError(StandardOutput.ToString(), Brushes.Gray);
-                foreach (var error in Installation.Errors)                
-                    PushError(error);                
+                //foreach (var error in Installation.Errors)                
+                //  PushError(error);                
                 var textBlock2 = new TextBlock()
                 {
-                    Text = $"=== FILES INSTALLED ======== \n{Installation.Manifest}",
+                    Text = $"\n=== FILES INSTALLED ======== \n{Installation.Manifest}",
                     Foreground = Brushes.Orange,
                     FontWeight = FontWeights.Bold,
                     TextWrapping = TextWrapping.NoWrap
@@ -296,12 +336,25 @@ namespace KrabbyQuestInstaller
                     TextWrapping = TextWrapping.Wrap
                 };
                 ErrorStack.Children.Add(textBlock1);
+                DumpLog(success);
                 if (!success)
                     ; // TODO: Non-successful installation help
                 ErrorScroller.ScrollToBottom();
                 LoadUserPrefs();
+                if (success)
+                    ShowSuccessMessage();
             });
-            
+        }
+
+        void DumpLog(bool success)
+        {
+            using (var file = File.CreateText("log.txt"))
+            {                
+                file.WriteLine("=========== INSTALL LOG " + DateTime.Now.ToString() + "===========");
+                file.WriteLine(Log.ToString());
+                file.WriteLine($"=== FILES INSTALLED ======== \n{Installation.Manifest}");
+                file.WriteLine($"Installation completed {(!success ? "un" : "")}successfully in " + Installation.ElapsedTime.ToString());
+            }
         }
 
         void SafePushError(string error, Brush foreground = default) => Dispatcher.Invoke(() => PushError(error, foreground));
@@ -317,12 +370,21 @@ namespace KrabbyQuestInstaller
                 TextWrapping = TextWrapping.Wrap
             };
             ErrorStack.Children.Add(textBlock);
+            Log.AppendLine(error);
         }
 
         void StepStarted(object s, StinkyFile.Installation.AutoInstall.InstallationType Type)
         {
             if (Type == AutoInstall.InstallationType.CreateModels)
-                Dispatcher.Invoke(PromptBlenderPlugin);                                                    
+                Dispatcher.Invoke(PromptBlenderPlugin);           
+            if (Type == AutoInstall.InstallationType.Finish)
+            {
+                if (isPartialInstall)
+                    return;
+                Installation.Paused = true;
+                //Patch krabby quest, then finally check the manifest in the finalization step
+                PatchKrabbyQuest(false).ContinueWith(delegate { Installation.Paused = false; });
+            }            
         }
 
         /// <summary>
@@ -363,11 +425,16 @@ namespace KrabbyQuestInstaller
             destinationBox.Text = path;
         }
 
-        private void PatchKrabbyQuest()
+        private async Task PatchKrabbyQuest(bool partial)
         {
-            var path = Path.Combine(WorkspaceDir.Text);            
+            string path = null;
+            Dispatcher.Invoke(delegate
+            {
+                path = Path.Combine(WorkspaceDir.Text);
+            });
+            if (path == null) return;
             GamePatcher patcher = new GamePatcher(path)
-            {                
+            {
                 ProgressChanged = (object s, (string m, double p) e) =>
                     InstallationInformationChanged(new InstallationCompletionInfo()
                     {
@@ -375,8 +442,49 @@ namespace KrabbyQuestInstaller
                         CurrentTask = e.m,
                         PercentComplete = e.p
                     }),
-            };            
-            Task.Run(patcher.Start);
+            };
+            if (partial)
+            {
+                isPartialInstall = true;
+                partialTitle = "Game Update";
+            }
+            if (Installation == null)
+            {
+                if (!partial) partial = true;
+                Installation = new AutoInstall()
+                {
+                    DestinationDirectory = path,
+                    OnProgressChanged = (object s, InstallationCompletionInfo info) => InstallationInformationChanged(info)
+                };
+            }
+            await Task.Run(() => patcher.Start(Installation)).ContinueWith((Task<bool> task) =>
+            {
+                if (task.IsFaulted)
+                    SafePushError(task.Exception.InnerException.Message);
+                if (partial)
+                {
+                    try
+                    {
+                        Installation.Start(AutoInstall.InstallationType.Finish, false);
+                    }
+                    catch (Exception e)
+                    {
+                        SafePushError(e.Message);
+                    }
+                    OnInstallationCompleted(task);
+                }
+                var settings = Properties.Settings.Default;
+                lock(settings)
+                {
+                    if (!settings.IsSynchronized)
+                        ;
+                    settings.GameDir = patcher.GameDir;
+                    settings.GameExePath = patcher.GameExePath;
+                    settings.EditorDir = patcher.EditorDir;
+                    settings.EditorExePath = patcher.EditorExePath;
+                    settings.Save();
+                }
+            });
         }
 
         private void StartInstallStep(object sender, System.Windows.Input.MouseButtonEventArgs e)
@@ -387,7 +495,7 @@ namespace KrabbyQuestInstaller
             if (sender.Equals(ModelsBorder)) type = AutoInstall.InstallationType.CreateModels;
             if (sender.Equals(PatchingBorder)) // patching step handled outside of StinkyFile
             {
-                PatchKrabbyQuest();
+                PatchKrabbyQuest(true);
                 return;
             }
             if (type != default)
@@ -414,6 +522,9 @@ namespace KrabbyQuestInstaller
                     break;
                 case InstallerState.WaitingForCompletion3D:
                     Installation.Paused = false;
+                    break;
+                case InstallerState.GoToLauncher:
+                    (Application.Current.MainWindow as MainWindow).SwitchScreen(MainWindow.ScreenState.Launcher);
                     break;
             }
             CurrentState = InstallerState.NoSpecialState;
@@ -456,8 +567,6 @@ namespace KrabbyQuestInstaller
             ShowMessage("Waiting for Blender...",
                         "The installation is paused. Once the Blender addon has been \n" +
                         "successfully installed, the 'All Set' button will be available. \n \n" +
-                        "The model installation will open command-windows one after another. \n" +
-                        "This is normal, but can be distracting. This process should not take long. \n \n" +
                         "If you do not want to install the 3D models right now, " +
                         "click 'Do it later'",
                         "All set", "Do it later");
@@ -466,7 +575,7 @@ namespace KrabbyQuestInstaller
         private void GetBlenderPlugin()
         {
             string blender = BlenderPath.Text;
-            Task.Run(delegate
+            Task.Run(async () =>
             {
                 DataReceivedEventHandler action = (object s, DataReceivedEventArgs e) => SafePushError(e.Data, Brushes.Gray);
                 //download plugin
@@ -474,10 +583,7 @@ namespace KrabbyQuestInstaller
                 if (!File.Exists(path))
                 {
                     using (var client = new WebClient())
-                    {
-                        client.DownloadFileAsync(
-                            new Uri("https://github.com/joric/io_scene_b3d/releases/download/1.0/io_scene_b3d.zip"),
-                            "plugin.zip");
+                    {                        
                         //progress changed
                         client.DownloadProgressChanged += async (object sender, DownloadProgressChangedEventArgs e) =>
                         {
@@ -489,22 +595,29 @@ namespace KrabbyQuestInstaller
                             };
                             InstallationInformationChanged(args);
                         };
-                        bool completed = false;
-                        client.DownloadFileCompleted += delegate { completed = true; };
-                        while (!completed) { } // wait for completion
+                        await client.DownloadFileTaskAsync(
+                            new Uri("https://github.com/joric/io_scene_b3d/releases/download/1.0/io_scene_b3d.zip"),
+                            "plugin.zip");
                     }
                 }
                 string pluginPath = Path.Combine(Environment.CurrentDirectory, "InstallPlugin.py");
-                PythonParameterEditor.ReplaceParameter(pluginPath, "filepath=", 0, path);
-                ProcessStartInfo startInfo = new ProcessStartInfo(blender, $"-b -P {path}")
+                PythonParameterEditor.ReplaceParameter(pluginPath, "filepath=", 0, path.Replace('\\', '/'));
+                ProcessStartInfo startInfo = new ProcessStartInfo(blender, $"-b -P {pluginPath}")
                 {
                     CreateNoWindow = true,
                     UseShellExecute = false,
                     RedirectStandardOutput = true
-                };                
+                };
                 var info = Process.Start(startInfo);
-                info.BeginOutputReadLine();
-                info.OutputDataReceived += action;
+                try
+                {                    
+                    info.BeginOutputReadLine();
+                    info.OutputDataReceived += action;
+                }
+                catch(Exception e)
+                {
+                    SafePushError(e.Message);
+                }
                 Dispatcher.Invoke(delegate
                 {
                     InstallationInformationChanged(new InstallationCompletionInfo()
@@ -516,10 +629,16 @@ namespace KrabbyQuestInstaller
                     WaitForUserCompletion();
                     MsgOKButton.IsEnabled = false;
                     MsgOKButton.Content = "Blender is open";
-                });
+                });                
                 info.WaitForExit();
-                info.CancelOutputRead();
-                info.OutputDataReceived -= action;
+                try
+                {
+                    info.CancelOutputRead();
+                    info.OutputDataReceived -= action;
+                }
+                catch(Exception e) {
+                    PushError(e.Message);
+                }
                 Dispatcher.Invoke(delegate
                 {
                     InstallationInformationChanged(new InstallationCompletionInfo()
@@ -541,6 +660,13 @@ namespace KrabbyQuestInstaller
                         MsgOKButton.Content = "Try again later";
                     }
                 });
+            }).ContinueWith((Task task) =>
+            {
+                if (task.IsFaulted)
+                {
+                    SafePushError(task.Exception.Flatten().InnerException.Message);
+                    Installation?.TogglePause();
+                }
             });
         }
         #endregion
